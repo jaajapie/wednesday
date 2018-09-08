05d383e74b67a254d8108f7522f65dda3fb94eaa
paramee <aepsrhunter@gmail.com>
2018-09-08 23:07:08 +0700

    commit

--- src/controllers/users/dataBase.js
+++ src/controllers/users/dataBase.js
@@ -1,13 +1,13 @@
-// let users = [{Id: 1, Name: 'Jang', Username: 'jang', Password: '1234'},
-//     {Id: 2, Name: 'Jang2', Username: 'jang2', Password: '1234'}]
+let users = [{Id: 1, Name: 'Jang', Username: 'jang', Password: '1234'},
+    {Id: 2, Name: 'Jang2', Username: 'jang2', Password: '1234'}]
 
 const ctrl = {}
 ctrl.GetUsers = async function (param) {
   return users
 },
 ctrl.PostUsers = async function (param) {
-  // let Id = users[users.length - 1].Id + 1
-  // users.push({ Id: Id, Name: param.Name, Username: param.Username, Password: param.Password })
+  let Id = users[users.length - 1].Id + 1
+  users.push({ Id: Id, Name: param.Name, Username: param.Username, Password: param.Password })
   return Id
 }
 
