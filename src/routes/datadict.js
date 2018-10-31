0bb3f2473d89e54640f50d0d70fbad5bd87d9a02
jang <kamonwan.sun@silkspan.local>
2018-10-31 16:58:09 +0700

    c

--- src/routes/datadict.js
+++ src/routes/datadict.js
@@ -8,22 +8,4 @@ router.get('/engine-ai/webhook', (req, res) => {
 router.post('/engine-ai/webhook', (req, res) => {
   datadict.EnginWebHook(req, res)
 })
-router.get('/servers', (req, res) => {
-  datadict.GetAllServer(req, res)
-})
-router.get('/servers/:id', (req, res) => {
-  datadict.GetAllTableByServerId(req, res)
-})
-router.get('/:id', (req, res) => {
-  datadict.GetDatadictByProjectId(req, res)
-})
-router.post('/:id/details', (req, res) => {
-  datadict.PostDatadictDetail(req, res)
-})
-router.post('/', (req, res) => {
-  datadict.PostDataDict(req, res)
-})
-router.put('/:id', (req, res) => {
-  datadict.PutDataDict(req, res)
-})
 module.exports = router
