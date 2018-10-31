33aeced975728948a3b7d85fd31015795770f20e
jang <kamonwan.sun@silkspan.local>
2018-10-31 17:00:54 +0700

    n

--- src/controllers/datadicts/dataBase.js
+++ src/controllers/datadicts/dataBase.js
@@ -1,65 +1,65 @@
-import gConfig from '../../dbHelper.js'
-import helper from '../../helper.js'
-import dbConfig from '../../../hiddenConfig/configDB'
+// import gConfig from '../../dbHelper.js'
+// import helper from '../../helper.js'
+// import dbConfig from '../../../hiddenConfig/configDB'
 
-class TableDAL {
-  GetAllServer () {
-    let data = dbConfig.GetAllServer()
-    return data
-  }
-  GetAllServerById (Id) {
-    let dataServer = dbConfig.GetAllServer()
-    let data = dataServer.filter((f) => {
-      return f.Id === 4
-    })
-    return data[0]
-  }
-  async GetAllTableByServerId (Id) {
-    let server = this.GetAllServerById(Id)
-    let serverDetail = dbConfig.GetServerDetail(server.Conn)
-    let query = `SELECT TABLE_NAME FROM ${serverDetail.database}.INFORMATION_SCHEMA.TABLES Order by TABLE_NAME ; `
-    let result = await gConfig.executeQueryAsync(serverDetail, query)
-    return result
-  }
-  async GetTableDetail (Id, param) {
-    let server = this.GetAllServerById(Id)
-    let serverDetail = dbConfig.GetServerDetail(server.Conn)
-    let query = `SELECT TABLE_NAME,COLUMN_NAME,ORDINAL_POSITION,COLUMN_DEFAULT,IS_NULLABLE,DATA_TYPE,CHARACTER_MAXIMUM_LENGTH,NUMERIC_PRECISION,NUMERIC_SCALE
-                 FROM INFORMATION_SCHEMA.COLUMNS
-                 WHERE TABLE_NAME in (${helper.setDefaultParam(param.tableNames, null)}); `
-    let result = await gConfig.executeQueryAsync(serverDetail, query)
-    return result
-  }
-  async GetTableProjectByProjectId (param) {
-    var query = `SELECT [Id]
-                ,[projectId]
-                ,[tableName]
-                ,[description]
-                FROM [test_project_table]
-                WHERE projectId = ${helper.setDefaultParam(param.projectId, null)}; `
-    let result = await gConfig.executeQueryAsync(dbConfig.connDev02, query)
-    return result
-  }
-  async PostTableProject (param) {
-    var query = `INSERT INTO [test_project_table]
-                ([projectId]
-                 [tableName]
-                ,[description])
-                 VALUES
-                (${helper.setDefaultParam(param.projectId, null)}
-                ,${helper.setDefaultParam(param.tableName, null)}
-                ,${helper.setDefaultParam(param.description, null)}); `
-    let result = await gConfig.executeQueryAsync(dbConfig.connDev02, query)
-    return result
-  }
-  async PutTableProject (param) {
-    var query = `UPDATE [test_project_table]
-                 SET  [tableName] = ${helper.setDefaultParam(param.tablename, null)}
-                 ,[description] = ${helper.setDefaultParam(param.description, null)}
-                 WHERE [projectId] = ${helper.setDefaultParam(param.projectId, null)}; `
-    let result = await gConfig.executeQueryAsync(dbConfig.connDev02, query)
-    return result
-  }
-}
+// class TableDAL {
+//   GetAllServer () {
+//     let data = dbConfig.GetAllServer()
+//     return data
+//   }
+//   GetAllServerById (Id) {
+//     let dataServer = dbConfig.GetAllServer()
+//     let data = dataServer.filter((f) => {
+//       return f.Id === 4
+//     })
+//     return data[0]
+//   }
+//   async GetAllTableByServerId (Id) {
+//     let server = this.GetAllServerById(Id)
+//     let serverDetail = dbConfig.GetServerDetail(server.Conn)
+//     let query = `SELECT TABLE_NAME FROM ${serverDetail.database}.INFORMATION_SCHEMA.TABLES Order by TABLE_NAME ; `
+//     let result = await gConfig.executeQueryAsync(serverDetail, query)
+//     return result
+//   }
+//   async GetTableDetail (Id, param) {
+//     let server = this.GetAllServerById(Id)
+//     let serverDetail = dbConfig.GetServerDetail(server.Conn)
+//     let query = `SELECT TABLE_NAME,COLUMN_NAME,ORDINAL_POSITION,COLUMN_DEFAULT,IS_NULLABLE,DATA_TYPE,CHARACTER_MAXIMUM_LENGTH,NUMERIC_PRECISION,NUMERIC_SCALE
+//                  FROM INFORMATION_SCHEMA.COLUMNS
+//                  WHERE TABLE_NAME in (${helper.setDefaultParam(param.tableNames, null)}); `
+//     let result = await gConfig.executeQueryAsync(serverDetail, query)
+//     return result
+//   }
+//   async GetTableProjectByProjectId (param) {
+//     var query = `SELECT [Id]
+//                 ,[projectId]
+//                 ,[tableName]
+//                 ,[description]
+//                 FROM [test_project_table]
+//                 WHERE projectId = ${helper.setDefaultParam(param.projectId, null)}; `
+//     let result = await gConfig.executeQueryAsync(dbConfig.connDev02, query)
+//     return result
+//   }
+//   async PostTableProject (param) {
+//     var query = `INSERT INTO [test_project_table]
+//                 ([projectId]
+//                  [tableName]
+//                 ,[description])
+//                  VALUES
+//                 (${helper.setDefaultParam(param.projectId, null)}
+//                 ,${helper.setDefaultParam(param.tableName, null)}
+//                 ,${helper.setDefaultParam(param.description, null)}); `
+//     let result = await gConfig.executeQueryAsync(dbConfig.connDev02, query)
+//     return result
+//   }
+//   async PutTableProject (param) {
+//     var query = `UPDATE [test_project_table]
+//                  SET  [tableName] = ${helper.setDefaultParam(param.tablename, null)}
+//                  ,[description] = ${helper.setDefaultParam(param.description, null)}
+//                  WHERE [projectId] = ${helper.setDefaultParam(param.projectId, null)}; `
+//     let result = await gConfig.executeQueryAsync(dbConfig.connDev02, query)
+//     return result
+//   }
+// }
 
-module.exports = TableDAL
+// module.exports = TableDAL
--- src/controllers/datadicts/index.js
+++ src/controllers/datadicts/index.js
@@ -1,9 +1,9 @@
-import DatadictBLL from './businessLogic'
-const helper = require('../../helper.js')
+// import DatadictBLL from './businessLogic'
+// const helper = require('../../helper.js')
 const token = 'test'
 class DatadictIndex {
   constructor () {
-    this.bll = new DatadictBLL()
+   // this.bll = new DatadictBLL()
   }
   async GetEnginWebHook (req, res) {
     if (req.query.token !== token) {
@@ -16,73 +16,73 @@ class DatadictIndex {
       return res.sendStatus(401)
     }
     return res.sendStatus(200)
-  }
-  async GetAllServer (req, res) {
-    let result = await this.bll.GetAllServer()
-    if (result.status === 200) {
-      helper.responseToClient({ res: res, httpcode: '200', data: result.data })
-    }
-  }
-  async GetAllTableByServerId (req, res) {
-    const {id} = req.params
-    let result = await this.bll.GetAllTableByServerId(id)
-    if (result.status === 200) {
-      helper.responseToClient({ res: res, httpcode: '200', data: result.data })
-    }
-  }
-  async PostDatadictDetail (req, res) {
-    const {id} = req.params
-    let result = await this.bll.GetDatadictDetail(id, req.body)
-    if (result.status === 200) {
-      helper.responseToClient({ res: res, httpcode: '200', data: result.data })
-    }
-  }
-  async GetDatadictByProjectId (req, res) {
-    const {id} = req.params
-    let result = await this.bll.GetAllTableByServerId(id)
-    if (result.status === 200) {
-      helper.responseToClient({ res: res, httpcode: '200', data: result.data })
-    } else {
-      helper.responseToClient({ res: res, httpcode: '204', msgerror: result.message })
-    }
-  }
-  async PostDataDict (req, res) {
-    if (req.body.data.length === 0) {
-      helper.responseToClient({ res: res, httpcode: '400', msgerror: 'Required array data' })
-    } else {
-      let dataProjectId = req.body.data.filter((f) => {
-        return f.projectId === '' || f.projectId === null
-      })
-      let dataTableName = req.body.data.filter((f) => {
-        return f.tableName === '' || f.tableName === null
-      })
-      if (dataTableName.length !== 0) {
-        helper.responseToClient({ res: res, httpcode: '400', msgerror: 'Required tableName' })
-      } else if (dataProjectId.length !== 0) {
-        helper.responseToClient({ res: res, httpcode: '400', msgerror: 'Required projectId' })
-      }
-    }
-    let result = await this.bll.PostDataDict()
-    if (result.status === 200) {
-      helper.responseToClient({ res: res, httpcode: '200', data: result.data })
-    }
-  }
-  async PutDataDict (req, res) {
-    if (req.body.data.length === 0) {
-      helper.responseToClient({ res: res, httpcode: '400', msgerror: 'Required array data' })
-    } else {
-      let dataTableName = req.body.data.filter((f) => {
-        return f.tableName === '' || f.tableName === null
-      })
-      if (dataTableName.length !== 0) {
-        helper.responseToClient({ res: res, httpcode: '400', msgerror: 'Required tableName' })
-      }
-    }
-    const {id} = req.params
-    let result = await this.bll.GetAllTableByServerId(id)
-    if (result.status === 200) {
-      helper.responseToClient({ res: res, httpcode: '200', data: result.data })
-    }
+  // }
+  // async GetAllServer (req, res) {
+  //   let result = await this.bll.GetAllServer()
+  //   if (result.status === 200) {
+  //     helper.responseToClient({ res: res, httpcode: '200', data: result.data })
+  //   }
+  // }
+  // async GetAllTableByServerId (req, res) {
+  //   const {id} = req.params
+  //   let result = await this.bll.GetAllTableByServerId(id)
+  //   if (result.status === 200) {
+  //     helper.responseToClient({ res: res, httpcode: '200', data: result.data })
+  //   }
+  // }
+  // async PostDatadictDetail (req, res) {
+  //   const {id} = req.params
+  //   let result = await this.bll.GetDatadictDetail(id, req.body)
+  //   if (result.status === 200) {
+  //     helper.responseToClient({ res: res, httpcode: '200', data: result.data })
+  //   }
+  // }
+  // async GetDatadictByProjectId (req, res) {
+  //   const {id} = req.params
+  //   let result = await this.bll.GetAllTableByServerId(id)
+  //   if (result.status === 200) {
+  //     helper.responseToClient({ res: res, httpcode: '200', data: result.data })
+  //   } else {
+  //     helper.responseToClient({ res: res, httpcode: '204', msgerror: result.message })
+  //   }
+  // }
+  // async PostDataDict (req, res) {
+  //   if (req.body.data.length === 0) {
+  //     helper.responseToClient({ res: res, httpcode: '400', msgerror: 'Required array data' })
+  //   } else {
+  //     let dataProjectId = req.body.data.filter((f) => {
+  //       return f.projectId === '' || f.projectId === null
+  //     })
+  //     let dataTableName = req.body.data.filter((f) => {
+  //       return f.tableName === '' || f.tableName === null
+  //     })
+  //     if (dataTableName.length !== 0) {
+  //       helper.responseToClient({ res: res, httpcode: '400', msgerror: 'Required tableName' })
+  //     } else if (dataProjectId.length !== 0) {
+  //       helper.responseToClient({ res: res, httpcode: '400', msgerror: 'Required projectId' })
+  //     }
+  //   }
+  //   let result = await this.bll.PostDataDict()
+  //   if (result.status === 200) {
+  //     helper.responseToClient({ res: res, httpcode: '200', data: result.data })
+  //   }
+  // }
+  // async PutDataDict (req, res) {
+  //   if (req.body.data.length === 0) {
+  //     helper.responseToClient({ res: res, httpcode: '400', msgerror: 'Required array data' })
+  //   } else {
+  //     let dataTableName = req.body.data.filter((f) => {
+  //       return f.tableName === '' || f.tableName === null
+  //     })
+  //     if (dataTableName.length !== 0) {
+  //       helper.responseToClient({ res: res, httpcode: '400', msgerror: 'Required tableName' })
+  //     }
+  //   }
+  //   const {id} = req.params
+  //   let result = await this.bll.GetAllTableByServerId(id)
+  //   if (result.status === 200) {
+  //     helper.responseToClient({ res: res, httpcode: '200', data: result.data })
+  //   }
   }
 }
 export default new DatadictIndex()
